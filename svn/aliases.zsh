b777d0a396e8e4eaf7689d6e443d50dc80e01712
Aaron Greenberg <a@aaronjgreenberg.com>
2012-08-31 14:09:14 -0400

    Add aliases for Subversion

---
+++ svn/aliases.zsh
@@ -0,0 +1,9 @@
+# SOME USEFUL SUBVERSION ALIASES
+# ==============================
+#
+# Aaron Greenberg
+# August 2012
+
+alias svns="svn status"		# Status of repo
+alias svna="svn add"			# Add file to repo
+alias svnc="svn ci"			# Commit file to repo
