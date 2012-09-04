5f70c7f40342979347bbd75918064b2e6b706607
Aaron Greenberg <a@aaronjgreenberg.com>
2012-09-04 16:02:12 -0400

    Add alias to list all of Olin printer URLs

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -4,8 +4,11 @@
 # Aaron Greenberg
 # August 2012
 
-alias h="cd"				# Single key to go home!
-alias c="clear"				# Clear the terminal.
-alias la="ls -al"			# Show long version of ALL the files.
-alias ll="ls -l"			# Show long version of files. No hidden tho.
-alias rl=". ~/.zshrc"		# Reload the environment.
+alias h="cd"								# Single key to go home!
+alias c="clear"								# Clear the terminal.
+alias la="ls -al"							# Show long version of ALL the files.
+alias ll="ls -l"							# Show long version of files. No hidden tho.
+alias rl=". ~/.zshrc"						# Reload the environment.
+
+# Get a list of Olin's printer URLs.
+alias printers='curl http://linux.olin.edu/printing/olin-cups.tar.gz --silent | gzip -d - | tar -Of - -x printers.conf | grep "<Printer" | cut -c 10- | sed "s/>\$/.olin.edu\//" | sed "s/^/http:\/\//"'
