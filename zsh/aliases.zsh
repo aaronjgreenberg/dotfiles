ad427ba5cbd3e7fa6267cd5664b8097ead28a8ee
Aaron Greenberg <a@aaronjgreenberg.com>
2013-02-27 00:47:37 -0500

    Fix typo

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -9,4 +9,4 @@ alias c="clear"								# Clear the terminal.
 alias la="ls -al"							# Show long version of ALL the files.
 alias ll="ls -l"							# Show long version of files. No hidden tho.
 alias rl=". ~/.zshrc"						# Reload the environment.
-alias codetime!="cd ~/Dropbox/Persona/code"
+alias codetime!="cd ~/Dropbox/Personal/code"
