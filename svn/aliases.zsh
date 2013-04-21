43cf01393f37e94614c5f45118cb066dde28d39b
Aaron Greenberg <a@aaronjgreenberg.com>
2013-04-21 13:55:24 -0400

    We're not using SVN anymore

--- svn/aliases.zsh
+++
@@ -1,9 +0,0 @@
-# SOME USEFUL SUBVERSION ALIASES
-# ==============================
-#
-# Aaron Greenberg
-# August 2012
-
-alias svns="svn status"		# Status of repo
-alias svna="svn add"			# Add file to repo
-alias svnc="svn ci"			# Commit file to repo
