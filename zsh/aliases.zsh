7b7f74a8923aa41ed8553949657cc2a4aa3ea53e
Aaron Greenberg <a@aaronjgreenberg.com>
2013-05-12 15:01:28 -0400

    Trim and clean zsh aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -4,9 +4,8 @@
 # Aaron Greenberg
 # August 2012
 
-alias h="cd"								# Single key to go home!
+alias h="cd"									# Single key to go home!
 alias c="clear"								# Clear the terminal.
 alias la="ls -al"							# Show long version of ALL the files.
 alias ll="ls -l"							# Show long version of files. No hidden tho.
-alias rl=". ~/.zshrc"						# Reload the environment.
-alias codetime!="cd ~/Dropbox/Personal/code"
+alias rl=". ~/.zshrc"					# Reload the environment.
