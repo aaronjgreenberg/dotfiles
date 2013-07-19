4dc70e3b0dcbcd49edc062712cfa9036a38b7901
Aaron Greenberg <a@aaronjgreenberg.com>
2013-07-19 19:02:06 -0400

    Fix up init.el to do autosaves correctly

--- emacs/emacs.d.symlink/init.el
+++ emacs/emacs.d.symlink/init.el
@@ -1,5 +1,11 @@
 ;; Save file backups in a centralized location
-(setq backup-directory-alist
-  `((".*" . ,"~/.emacs.d/.saves")))
-(setq auto-save-file-name-transforms
-	`((".*" ,"~/.emacs.d/.saves" t)))
+(setq backup-directory-alist `(("." . "~/.emacs.d/.saves/")))
+(setq backup-by-copying t)
+(setq delete-old-versions t
+  kept-new-versions 6
+  kept-old-versions 2)
+
+(require 'package)
+(add-to-list 'package-archives
+             '("marmalade" . "http://marmalade-repo.org/packages/") t)
+(package-initialize)
